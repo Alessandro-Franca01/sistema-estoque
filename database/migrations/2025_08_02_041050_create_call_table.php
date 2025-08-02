9e4689cbdff6c3167de190a8923694b1d1448b7c
Alessandro-Franca01 <alessandro0564@yahoo.com.br>
2025-08-02 02:20:02 -0300

    feat: Create migration to Call

---
+++ database/migrations/2025_08_02_041050_create_call_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('call', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', ['whatssap', 'conectar_cabedelo', 'personally', 'phone', 'other'])->default('conectar_cabedelo');
+            $table->string('service_order')->nullable();
+            $table->string('connect_code')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('caller_name')->nullable();
+            $table->text('observation')->nullable();
+            $table->foreignId('output_id')->constrained()->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('call');
+    }
+};
