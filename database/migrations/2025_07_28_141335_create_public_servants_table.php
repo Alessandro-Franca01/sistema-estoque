02eb54846ae116fd1c12530e95110df692f41aac
Alessandro-Franca01 <alessandro0564@yahoo.com.br>
2025-09-30 03:03:45 -0300

    feat: Ajustments in migrationsclear

--- database/migrations/2025_07_28_141335_create_public_servants_table.php
+++ database/migrations/2025_07_28_141335_create_public_servants_table.php
@@ -17,10 +17,7 @@ return new class extends Migration
             $table->string('phone')->nullable();
             $table->string('department')->nullable();
             $table->string('position')->nullable();
-            $table->enum('job_function', ['ADMINISTRADOR', 'ALMOXARIFE', 'OPERADOR', 'SERVIDOR'])->default('SERVIDOR');
-            $table->boolean('active')->default(true);
             $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
-            $table->foreignId('department_id')->constrained('departments');
             $table->timestamps();
         });
     }
---
+++ database/migrations/2025_07_28_141336_create_public_servant_departments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('public_servant_departments', function (Blueprint $table) {
+            $table->id();
+            $table->enum('job_function', ['ADMINISTRADOR', 'ALMOXARIFE', 'OPERADOR', 'SERVIDOR'])->default('SERVIDOR');
+            $table->foreignId('public_servant_id')->constrained('public_servants')->onDelete('cascade');
+            $table->foreignId('department_id')->constrained('departments')->onDelete('cascade');
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+            $table->unique(['public_servant_id', 'department_id'], 'ps_department_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('public_servant_departments');
+    }
+};
